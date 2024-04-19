4a7b905aac2bd0ae5b9f06b1849ae21cf8e90191
Alco80 <leeshla2525@gmail.com>
2024-04-18 22:30:49 -0400

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,26 +1,25 @@
+import { Link } from 'react-router-dom';
+
 const Header = () => {
 return(
 <div>
 
 <nav class="navbar navbar-expand-lg bg-success">
   <div className="container-fluid">
-    <a className="navbar-brand" href="#">Navbar</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
+    <Link className="navbar-brand text-light fs-1 fw-bold" to="/">My Web Solutions</Link>
+    <button className="navbar-toggler text-light" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon text-light"></span>
     </button>
-    <div className="collapse navbar-collapse" id="navbarNav">
-      <ul className="navbar-nav">
+    <div className="collapse navbar-collapse text-light" id="navbarNav">
+      <ul className="navbar-nav p-3">
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">Home</a>
+          <Link className="nav-link active text-light" aria-current="page" to="/">Home</Link>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">Features</a>
+          <Link className="nav-link text-light" to="/Solutions">Solutions</Link>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">Pricing</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link disabled">Disabled</a>
+          <Link className="nav-link text-light" to="/Contact">Contact</Link>
         </li>
       </ul>
     </div>
