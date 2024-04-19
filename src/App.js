2938ef11ed16ed1300d0eb5d1e1bb6b1f8605a52
Alco80 <leeshla2525@gmail.com>
2024-04-18 22:07:30 -0400

    Lots of pages added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
+import * as React from 'react'
+import { Routes, Route } from 'react-router-dom';
+// Components 
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+// Pages
+
+import Home from './pages/Home';
+import Solutions from './pages/Solutions';
 
 import './App.css';
 
 function App() {
   return (
     <div className="app">
-
+<Routes>
+  <Route />
+  <Route />
+  <Route />
+  <Route />
+</Routes>
     </div>
   );
 }
